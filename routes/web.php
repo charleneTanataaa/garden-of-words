41bba076796daa22195af7803569af3130da7b43
chelsyCy <chelsytioyan@gmail.com>
2025-07-01 23:44:30 +0700

    kerangka (error no database)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomePageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomePageController::class, 'index']);
