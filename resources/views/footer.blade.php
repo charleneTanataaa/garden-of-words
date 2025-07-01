63393c008e93f2a3913437c548e9af14f33cbaf4
Your Name <charlenetanataaa@gmail.com>
2025-07-01 18:40:49 +0700

    app + footer template

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,26 @@
+
+</head>
+<body>
+  <footer>
+    <div class="footer-container">
+      <div class="footer-left">
+        <img src="./logo.png" alt="Illustration" />
+      </div>
+      <div class="footer-middle">
+        <h2><strong>Garden of Words</strong></h2>
+        <p>Write letters and grow your favourite plants!</p>
+      </div>
+      <div class="footer-right">
+        <h3><strong>Links</strong></h3>
+        <div class="footer-links">
+          <a href="#">Home</a>
+          <a href="#">Pages</a>
+          <a href="#">About Us</a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      &copy;2025 Garden of Words. All right reserved
+    </div>
+  </footer>
+
