390fcb5c1ace07aaa09558df1476409f4297fcea
Your Name <charlenetanataaa@gmail.com>
2025-07-09 15:07:57 +0700

    fix logout + signup logos

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg" style="background-color: var(--background-color); padding: 10px 20px;">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">
-                <img src="/images/logo.png" alt="logo" style="width: 60px;">
+                <img src="/images/main_logo.png" alt="logo" style="width: 60px;">
             </a>
 
             {{-- <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -19,8 +19,21 @@
                     <li class="nav-item">
                         <a class="nav-link text-white" href="/about">About us</a>
                     </li>
+
                     <li class="nav-item">
+
+                        @if(Auth::check())
+                            <li class="nav-item">
+                                @if(Auth::check())
+                                <form action="{{ route('logout') }}" method="POST" style="display:inline">
+                                    @csrf
+                                    <button type="submit" class="btn" style="background-color: #F8F3BA;">Logout</button>
+                                </form>
+                                @endif
+                            </li>
+                        @else
                         <a class="btn" style="background-color: #F8F3BA;" href={{ route('register.email.form') }}>Sign Up</a>
+                        @endif
                     </li>
                 </ul>
             </div>
